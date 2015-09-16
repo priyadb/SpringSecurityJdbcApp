5c7d8e6ced93cc63fafc34db1dfa230e4a6979f7
Priyadarshini Balachandran <dharshus.1989@gmail.com>
2015-09-16 16:33:06 -0500

    Spring Security 4 - Simple JDBC Authentication and Authorization
    Spring Security 4 - Simple JDBC Authentication and Authorization

---
+++ src/main/java/hello/MvcConfig.java
@@ -0,0 +1,44 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/403").setViewName("403");
+    }
+    
+    @Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+	    DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+	    driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+	    driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/userbase");
+	    driverManagerDataSource.setUsername("root");
+	    driverManagerDataSource.setPassword("root");
+	    return driverManagerDataSource;
+	}
+    
+    @Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+    
+        
+   
+    
+     
+}
